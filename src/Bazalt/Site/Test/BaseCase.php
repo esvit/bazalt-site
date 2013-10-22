10c59831d5ac273691a395bc1caa7f0d1faa98bb
Vitalii Savchuk <esvit666@gmail.com>
2013-10-22 21:38:47 +0300

    fix test

--- src/Bazalt/Site/Test/BaseCase.php
+++ src/Bazalt/Site/Test/BaseCase.php
@@ -15,18 +15,6 @@ abstract class BaseCase extends \Bazalt\Rest\Test\BaseCase
         $this->site = \Bazalt\Site\Model\Site::create();
         $this->site->save();
 
-        $user = \Bazalt\Auth\Model\User::getUserByLogin('Test');
-        if ($user) {
-            $user->delete();
-        }
-
-        $this->user = \Bazalt\Auth\Model\User::create();
-        $this->user->login = 'Test';
-        $this->user->is_active = 1;
-        $this->user->save();
-
-        \Bazalt\Auth::setUser($this->user);
-
         \Bazalt\Site::setCurrent($this->site);
     }
 
@@ -35,12 +23,7 @@ abstract class BaseCase extends \Bazalt\Rest\Test\BaseCase
         if ($this->site->id) {
             $this->site->delete();
         }
-        if ($this->user->id) {
-            $this->user->delete();
-        }
         $this->site = null;
-        $this->user = null;
         \Bazalt\Site::setCurrent(null);
-        \Bazalt\Auth::setUser(null);
     }
 }
